be82fd5cf1a53ead96a25963f417f907b62c5d08
Matheus Fellipe <matheus060399@gmail.com>
2022-06-13 13:13:37 -0400

    config express app

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,13 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as logger from 'morgan';
+import * as swagger from 'swagger-node-express';
+
+export const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+
+swagger.setAppHandler(app);
