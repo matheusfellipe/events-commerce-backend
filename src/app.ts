29fbad2df4091b7ea9f16c97fac89f2dff741a78
Matheus Fellipe <matheus060399@gmail.com>
2022-06-13 14:54:23 -0400

    improving routes

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,30 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as logger from 'morgan';
-import * as swaggerUi from 'swagger-ui-express';
-import * as swaggerDocument from 'swagger-jsdoc'
+// import * as swaggerUi from 'swagger-ui-express';
+// import * as swaggerDocument from 'swagger-jsdoc'
 
 import {routerUser} from './routes/user';
 
 export const app = express();
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Library API',
-            version: '1.0.0'
-        }
-    },
-    apis: ['app.js']
-}
+// const swaggerOptions = {
+//     swaggerDefinition: {
+//         info: {
+//             title: 'Library API',
+//             version: '1.0.0'
+//         }
+//     },
+//     apis: ['app.js']
+// }
 
-const swaggerDocs = swaggerDocument(swaggerOptions);
+// const swaggerDocs = swaggerDocument(swaggerOptions);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+// app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
 app.use('/user',routerUser);
 app.use('/',(req,res)=> res.send('API of app Events Commerce'));
 
---
+++ src/controllers/userController.ts
@@ -0,0 +1,5 @@
+import { User } from "../models/User";
+
+export class userController {
+    async save(user: User){}
+}
